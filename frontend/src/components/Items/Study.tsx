c7b8579efe467c7c3b5370958b50926192b2c042
jonas <chenshuaiyuan@ikingcity.com>
2026-01-15 16:52:26 +0800

    initial commit

---
+++ frontend/src/components/Items/Study.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react"
+import { z } from "zod"
+
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select"
+
+// 使用zod来约束value的类型
+const formSchema = z.object({
+    value: z.string(),
+
+})
+
+const propsSchema = z.object({
+    value_data: z.object({
+        name: z.string(),
+        age: z.number(),
+    }).optional()
+})
+
+// infer是zod的类型推断工具，可以推断出value的类型
+type FormData = z.infer<typeof formSchema>
+type PropsData = z.infer<typeof propsSchema>
+
+const Study = (data: PropsData) => {
+    const [value, setValue] = useState<FormData>({
+        value: "你好哇",
+    })
+    useEffect(() => {
+        console.log(data)
+    }, [])
+    return (
+        <div>
+            <Select value={value.value} onValueChange={(value) => setValue({ value })}>
+                <SelectTrigger>
+                    <SelectValue placeholder="Select a value" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectItem value="value1">Value 1</SelectItem>
+                    <SelectItem value="value2">Value 2</SelectItem>
+                    <SelectItem value="value3">Value 3</SelectItem>
+                </SelectContent>
+            </Select>
+        </div>
+    )
+}
+
+export default Study
